8a3e7abbb4a966511d762170872d1974f4a2c4ae
Rehan Ahmed <ra4820340@gmail.com>
2026-09-17 14:59:08 +0530

    Connect frontend to live backend

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/notes";
+const API_URL = "https://quick-note-app-dboa.onrender.com/notes";
 
 const noteForm = document.getElementById("noteForm");
 const titleInput = document.getElementById("title");
